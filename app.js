de3da3ea1e4f8c5c968fc439acde74dbefd2f6be
James McBeath <jamesmcbeath@me.com>
2023-01-09 17:05:47 +0000

    Function showQuestion added

--- app.js
+++ app.js
@@ -25,3 +25,16 @@ function setNextQuestion() {
   showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
